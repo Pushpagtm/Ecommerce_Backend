3d8be92b6a6ede4bb4621f958fb56e546d5318a3
Pushpagtm <pushpagautam529@gmail.com>
2023-07-15 21:36:17 +0545

    work

--- controller/Product.js
+++ controller/Product.js
@@ -11,3 +11,47 @@ exports.createProduct = async (req, res) => {
     res.status(400).json(err);
   }
 };
+exports.fetchAllProducts = async (req, res) => {
+  let condition = {}
+  if(!req.query.admin){
+      condition.deleted = {$ne:true}
+  }
+  
+  let query = Product.find(condition);
+  let totalProductsQuery = Product.find(condition);
+
+  console.log(req.query.category);
+
+  if (req.query.category) {
+    query = query.find({ category: {$in:req.query.category.split(',')} });
+    totalProductsQuery = totalProductsQuery.find({
+      category: {$in:req.query.category.split(',')},
+    });
+  }
+  if (req.query.brand) {
+    query = query.find({ brand: {$in:req.query.brand.split(',')} });
+    totalProductsQuery = totalProductsQuery.find({ brand: {$in:req.query.brand.split(',') }});
+  }
+  if (req.query._sort && req.query._order) {
+    query = query.sort({ [req.query._sort]: req.query._order });
+  }
+
+  // const totalDocs = await totalProductsQuery.count().exec();
+  // console.log({ totalDocs });
+
+  if (req.query._page && req.query._limit) {
+    const pageSize = req.query._limit;
+    const page = req.query._page;
+    query = query.skip(pageSize * (page - 1)).limit(pageSize);
+  }
+
+  try {
+    const docs = await query.exec();
+    // res.set('X-Total-Count', totalDocs);
+    res.status(200).json(docs);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+
--- routes/Products.js
+++ routes/Products.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const { createProduct } = require('../controller/Product');
-// const { Product } = require('../model/Product');
+const express = require("express");
+const { createProduct, fetchAllProducts } = require("../controller/Product");
 
 const router = express.Router();
 //  /products is already added in base path
-router.post('/', createProduct)
-     
-      
+router.post("/", createProduct).get("/", fetchAllProducts);
 
 exports.router = router;
